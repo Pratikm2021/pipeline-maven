5c05d1f7a39ca1f9732665fee6575e501999e846
pratikm2021 <mohanty.pratik2021@gmail.com>
2022-04-17 22:01:14 +0530

    Fixes Workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "*********BUILDING JAR*************"
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@" -Denforcer.skip=true
+
+WORKSPACE=/home/administrator/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@" -Denforcer.skip=true
