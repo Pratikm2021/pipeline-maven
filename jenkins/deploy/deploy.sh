1aa89ec6adbb3191b8a1b51471da02b13c050f14
pratikm2021 <mohanty.pratik2021@gmail.com>
2022-04-17 22:58:38 +0530

    Changed the ubuntu.pem path in deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,5 +7,7 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/administrator/DevOps/ubuntu.pem /tmp/.auth ubuntu@13.126.149.60:/tmp/.auth
-scp -i /home/administrator/DevOps/ubuntu.pem ./jenkins/deploy/publish.sh ubuntu@13.126.149.60:/tmp/publish.sh
+#WORKSPACE=/home/administrator/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+scp -i /opt/ubuntu.pem /tmp/.auth ubuntu@13.126.149.60:/tmp/.auth
+scp -i /opt/ubuntu.pem ./jenkins/deploy/publish.sh ubuntu@13.126.149.60:/tmp/publish.sh
